9695fd923ffec352806db7aafd75ad1f3f611434
xuexiao <xuexiao@belink.com>
2021-11-18 16:32:31 +0800

    second commit

---
+++ simplespringboot/src/main/java/org/cloudxue/simplespringboot/MyApplication.java
@@ -0,0 +1,18 @@
+package org.cloudxue.simplespringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @ClassName MyApplication
+ * @Description 请描述类的业务用途
+ * @Author xuexiao
+ * @Date 2021/11/18 上午11:22
+ * @Version 1.0
+ **/
+@SpringBootApplication
+public class MyApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MyApplication.class, args);
+    }
+}
