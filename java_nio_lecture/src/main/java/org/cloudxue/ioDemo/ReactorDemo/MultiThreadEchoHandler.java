cf93ce7638925c6b9fe71f43247f1076e992aa33
xuexiao <xuexiao@belink.com>
2021-11-30 15:43:31 +0800

    Reactor模式多线程版本实现

--- java_nio_lecture/src/main/java/org/cloudxue/ioDemo/ReactorDemo/MultiThreadEchoHandler.java
+++ java_nio_lecture/src/main/java/org/cloudxue/ioDemo/ReactorDemo/MultiThreadEchoHandler.java
@@ -1,5 +1,16 @@
 package org.cloudxue.ioDemo.ReactorDemo;
 
+import lombok.extern.slf4j.Slf4j;
+import org.cloudxue.NioDemoConfig;
+
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.channels.SelectionKey;
+import java.nio.channels.Selector;
+import java.nio.channels.SocketChannel;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 /**
  * @ClassName MultiThreadEchoHandler
  * @Description 请描述类的业务用途
@@ -7,6 +18,71 @@ package org.cloudxue.ioDemo.ReactorDemo;
  * @Date 2021/11/29 下午3:25
  * @Version 1.0
  **/
-public class MultiThreadEchoHandler {
+@Slf4j
+public class MultiThreadEchoHandler implements Runnable{
+    final SocketChannel channel;
+    final SelectionKey sk;
+    final ByteBuffer byteBuffer = ByteBuffer.allocate(NioDemoConfig.SEND_BUFFER_SIZE);
+    static final int RECEIVING = 0, SENDING = 1;
+    int status = RECEIVING;
+    //引入线程池
+    static ExecutorService pool = Executors.newFixedThreadPool(4);
 
+    MultiThreadEchoHandler(Selector selector, SocketChannel channel) throws IOException {
+        this.channel = channel;
+        channel.configureBlocking(false);
+        //先取得选择键，再设置感兴趣的IO事件
+        sk = channel.register(selector, 0);
+        //将自身Handler作为sk选择键的附件，方便事件dispatch
+        sk.attach(this);
+        //向sk选择键设置READ就绪事件
+        sk.interestOps(SelectionKey.OP_READ);
+        selector.wakeup();
+    }
+    //此run方法在IO轮询事件中被调用
+    @Override
+    public void run() {
+//        pool.submit(() -> asyncRun());
+        pool.execute(new AsyncTask());
+    }
+
+    public synchronized  void asyncRun() {
+        try {
+            if (status == SENDING) {
+                //写入通道
+                channel.write(byteBuffer);
+                //写完成，准备开始从通道读，byteBuffer切换成写模式
+                byteBuffer.clear();
+                //写完成，注册READ就绪事件
+                sk.interestOps(SelectionKey.OP_READ);
+                //写完成，进入接收状态
+                status = RECEIVING;
+            } else if (status == RECEIVING) {
+                int readLen = 0;
+                while ((readLen = channel.read(byteBuffer)) > 0) {
+                    log.info(new String(byteBuffer.array(), 0, readLen));
+                }
+                //读完成，准备开始写入通道，byteBuffer切换成读模式
+                byteBuffer.flip();
+                //读完成，注册WRITE就绪事件
+                sk.interestOps(SelectionKey.OP_WRITE);
+                //读完成，进入发送状态
+                status = SENDING;
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            sk.cancel();
+            try {
+                channel.finishConnect();
+            } catch (IOException ex) {
+                ex.printStackTrace();
+            }
+        }
+    }
+
+    class AsyncTask implements Runnable {
+        public void run () {
+            MultiThreadEchoHandler.this.asyncRun();
+        }
+    }
 }
--- java_nio_lecture/src/main/java/org/cloudxue/ioDemo/ReactorDemo/MultiThreadEchoServerReactor.java
+++ java_nio_lecture/src/main/java/org/cloudxue/ioDemo/ReactorDemo/MultiThreadEchoServerReactor.java
@@ -1,12 +1,119 @@
 package org.cloudxue.ioDemo.ReactorDemo;
 
+import lombok.extern.slf4j.Slf4j;
+import org.cloudxue.NioDemoConfig;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.nio.channels.SelectionKey;
+import java.nio.channels.Selector;
+import java.nio.channels.ServerSocketChannel;
+import java.nio.channels.SocketChannel;
+import java.util.Iterator;
+import java.util.Set;
+import java.util.concurrent.atomic.AtomicInteger;
+
 /**
  * @ClassName MultiThreadEchoServerReactor
- * @Description 请描述类的业务用途
+ * @Description 多线程版本反应器模式实例
+ * 两个选择器、两个子反应器，每个子反应器负责查询和分发一个选择器的IO事件
+ * 服务端的监听通道（ServerSocketChannel）都注册到第一个选择器
+ * Socket传输通道（SocketChannel）都注册到第二个选择器
+ * 实现了【新连接监听】和【IO读写事件监听】的线程分离
  * @Author xuexiao
  * @Date 2021/11/29 下午3:00
  * @Version 1.0
  **/
+@Slf4j
 public class MultiThreadEchoServerReactor {
+    ServerSocketChannel serverSocketChannel;
+    AtomicInteger next = new AtomicInteger(0);
+    //选择器数组，引入多个选择器
+    Selector[] selectors = new Selector[2];
+    //引入多个子反应器
+    SubSelector[] subSelectors = null;
 
+    MultiThreadEchoServerReactor() throws IOException {
+        //初始化多个选择器
+        selectors[0] = Selector.open();
+        selectors[1] = Selector.open();
+        serverSocketChannel = ServerSocketChannel.open();
+        serverSocketChannel.configureBlocking(false);
+        InetSocketAddress address = new InetSocketAddress(NioDemoConfig.SOCKET_SERVER_IP, NioDemoConfig.SOCKET_SERVER_PORT);
+        serverSocketChannel.socket().bind(address);
+        log.info("服务端已开始监听: " + address);
+        //第一个选择器，负责监听新连接事件
+        SelectionKey sk = serverSocketChannel.register(selectors[0], SelectionKey.OP_ACCEPT);
+        //绑定Handler：新连接监控Handler绑定到SelectionKey
+        sk.attach(new AcceptorHandler());
+        //第一个子反应器，负责第一个选择器的新连接事件分发（而不处理）
+        SubSelector subSelector1 = new SubSelector(selectors[0]);
+        //第二个子反应器，负责第二个选择器的传输事件分发（而不处理）
+        SubSelector subSelector2 = new SubSelector(selectors[1]);
+        subSelectors = new SubSelector[]{subSelector1, subSelector2};
+    }
+
+    private void startServer() {
+        new Thread(subSelectors[0]).start();
+        new Thread(subSelectors[1]).start();
+    }
+
+    //子反应器，负责事件分发，但不负责事件处理
+    class SubSelector implements Runnable {
+        final Selector selector;
+
+        SubSelector(Selector selector) {
+            this.selector = selector;
+        }
+
+        @Override
+        public void run() {
+            try {
+                while (!Thread.interrupted()) {
+                    selector.select();
+                    Set<SelectionKey> selectionKeys = selector.selectedKeys();
+                    Iterator<SelectionKey> iterator = selectionKeys.iterator();
+                    while (iterator.hasNext()) {
+                        SelectionKey sk = iterator.next();
+                        if (null != sk) {
+                            dispatch(sk);
+                        }
+                    }
+                    selectionKeys.clear();
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    void dispatch(SelectionKey sk) {
+        Runnable handler = (Runnable) sk.attachment();
+        if (null != handler) {
+            handler.run();
+        }
+    }
+
+    class AcceptorHandler implements Runnable {
+        @Override
+        public void run() {
+            try {
+                SocketChannel socketChannel = serverSocketChannel.accept();
+                if (null != socketChannel) {
+                    log.info("接收到一个新连接");
+                    int index = next.get();
+                    log.info("选择器的编号： " + index);
+                    Selector selector = selectors[index];
+                    new MultiThreadEchoHandler(selector, socketChannel);
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public static void main(String[] args) throws IOException{
+        MultiThreadEchoServerReactor server = new MultiThreadEchoServerReactor();
+        server.startServer();
+    }
 }
