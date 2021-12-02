fe5e7d5d6286b47af04a7ebd78384f8824340145
xuexiao <xuexiao@belink.com>
2021-12-02 17:29:21 +0800

    Buffer中的Scathering与Gathering示例

---
+++ java_nio_lecture/src/main/java/org/cloudxue/ioDemo/fileDemo/FileChannelDemo.java
@@ -0,0 +1,12 @@
+package org.cloudxue.ioDemo.fileDemo;
+
+/**
+ * @ClassName FileChannelDemo
+ * @Description 请描述类的业务用途
+ * @Author xuexiao
+ * @Date 2021/12/1 上午11:12
+ * @Version 1.0
+ **/
+public class FileChannelDemo {
+
+}
