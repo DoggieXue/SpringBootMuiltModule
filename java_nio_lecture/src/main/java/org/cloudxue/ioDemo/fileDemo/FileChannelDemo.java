d2e97be5570adfe1b2e2db0c4d721c2cf96293ee
xuexiao <xuexiao@belink.com>
2021-12-02 17:30:04 +0800

    Buffer中的堆外内存示例

--- java_nio_lecture/src/main/java/org/cloudxue/ioDemo/fileDemo/FileChannelDemo.java
+++ java_nio_lecture/src/main/java/org/cloudxue/ioDemo/fileDemo/FileChannelDemo.java
@@ -1,5 +1,14 @@
 package org.cloudxue.ioDemo.fileDemo;
 
+import org.cloudxue.NioDemoConfig;
+import org.cloudxue.common.util.IOUtil;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.channels.FileChannel;
+
 /**
  * @ClassName FileChannelDemo
  * @Description 请描述类的业务用途
@@ -8,5 +17,28 @@ package org.cloudxue.ioDemo.fileDemo;
  * @Version 1.0
  **/
 public class FileChannelDemo {
+    public static void main(String[] args) throws IOException {
+        FileInputStream fileInputStream = new FileInputStream(IOUtil.getResourcePath(NioDemoConfig.FILE_RESOURCE_SRC_PATH));
+        FileOutputStream fileOutputStream = new FileOutputStream(IOUtil.getResourcePath(NioDemoConfig.FILE_RESOURCE_DEST_PATH));
 
+        FileChannel inputChannel = fileInputStream.getChannel();
+        FileChannel outputChannel = fileOutputStream.getChannel();
+
+        ByteBuffer buffer = ByteBuffer.allocate(1024);
+        ByteBuffer bytebuffer = ByteBuffer.allocateDirect(1024);
+        while (true) {
+            //若该行代码注释掉会发生什么？
+            buffer.clear();
+            int readLen = inputChannel.read(buffer);
+            if (readLen == -1) {
+                break;
+            }
+            System.out.println("read: " + readLen);
+            buffer.flip();
+
+            outputChannel.write(buffer);
+        }
+        outputChannel.close();
+        inputChannel.close();
+    }
 }
