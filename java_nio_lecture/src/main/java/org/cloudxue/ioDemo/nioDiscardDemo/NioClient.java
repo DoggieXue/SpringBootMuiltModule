5d37f6e1026f6e97caaea519f2683f6388d18a21
xuexiao <xuexiao@belink.com>
2021-12-06 10:32:52 +0800

    NIO实现服务端广播功能

---
+++ java_nio_lecture/src/main/java/org/cloudxue/ioDemo/nioDiscardDemo/NioClient.java
@@ -0,0 +1,85 @@
+package org.cloudxue.ioDemo.nioDiscardDemo;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.InetSocketAddress;
+import java.nio.ByteBuffer;
+import java.nio.channels.SelectionKey;
+import java.nio.channels.Selector;
+import java.nio.channels.SocketChannel;
+import java.time.LocalDateTime;
+import java.util.Set;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+/**
+ * @ClassName NioClient
+ * @Description 向服务端发送信息，并接收服务端发送的信息
+ * @Author xuexiao
+ * @Date 2021/12/5 下午8:37
+ * @Version 1.0
+ **/
+public class NioClient {
+    public static void main(String[] args) {
+        try {
+            SocketChannel socketChannel = SocketChannel.open();
+            socketChannel.configureBlocking(false);
+
+            Selector selector = Selector.open();
+            socketChannel.register(selector, SelectionKey.OP_CONNECT);
+            socketChannel.connect(new InetSocketAddress("127.0.0.1", 8899));
+
+            while (true) {
+                selector.select();
+                Set<SelectionKey> selectionKeys = selector.selectedKeys();
+
+                for (SelectionKey selectionKey : selectionKeys) {
+                    if (selectionKey.isConnectable()) {
+                        SocketChannel client = (SocketChannel) selectionKey.channel();
+                        if (client.isConnectionPending()) {
+                            client.finishConnect();//连接真正建立好
+                            ByteBuffer writeBuffer = ByteBuffer.allocate(1024);
+                            writeBuffer.put((LocalDateTime.now() + "连接成功").getBytes());
+                            writeBuffer.flip();
+                            client.write(writeBuffer);
+
+                            ExecutorService executorService = Executors.newSingleThreadExecutor(Executors.defaultThreadFactory());
+                            executorService.submit(() -> {
+                               while (true) {
+                                   try {
+                                       writeBuffer.clear();
+                                       InputStreamReader input = new InputStreamReader(System.in);
+                                       BufferedReader br = new BufferedReader(input);
+
+                                       String message = br.readLine();
+
+                                       writeBuffer.put(message.getBytes());
+                                       writeBuffer.flip();
+                                       client.write(writeBuffer);
+                                   } catch (Exception ex) {
+                                       ex.printStackTrace();
+                                   }
+                               }
+                            });
+                        }
+                        client.register(selector, SelectionKey.OP_READ);
+                    } else if (selectionKey.isReadable()) {
+                        SocketChannel client = (SocketChannel)selectionKey.channel();
+
+                        ByteBuffer readBuffer = ByteBuffer.allocate(1024);
+
+                        int count = client.read(readBuffer);
+
+                        if (count > 0) {
+                            String receivedMsg = new String(readBuffer.array(), 0, count);
+                            System.out.println(receivedMsg);
+                        }
+                    }
+                }
+                selectionKeys.clear();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
