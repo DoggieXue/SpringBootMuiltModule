431affab4f1e1ab7991e8bb7fbe64cb435d58761
xuexiao <xuexiao@belink.com>
2022-04-10 12:11:16 +0800

    Excel读取工具类

---
+++ democommon/src/main/java/org/cloudxue/common/util/ExcelUtil.java
@@ -0,0 +1,13 @@
+package org.cloudxue.common.util;
+
+/**
+ * @ClassName ExcelUtil
+ * @Description 请描述类的业务用途
+ * @Author xuexiao
+ * @Date 2022/4/7 上午10:09
+ * @Version 1.0
+ **/
+public class ExcelUtil {
+    public static void simpleRead(String filePath) {
+    }
+}
