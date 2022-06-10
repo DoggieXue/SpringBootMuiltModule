cf358076d629c78dd2bb624d528fc23af6516db2
xuexiao <xuexiao@belink.com>
2022-06-10 17:29:28 +0800

    并发编程三大问题示例代码

---
+++ multi_thread_lecture/src/main/java/org/cloudxue/visiable/CounterSample.java
@@ -0,0 +1,16 @@
+package org.cloudxue.visiable;
+
+/**
+ * @ClassName CounterSample
+ * @Description 请描述类的业务用途
+ * @Author xuexiao
+ * @Date 2022/6/9 5:43 下午
+ * @Version 1.0
+ **/
+public class CounterSample {
+    int i = 0;
+
+    public void increase() {
+        i++;
+    }
+}
