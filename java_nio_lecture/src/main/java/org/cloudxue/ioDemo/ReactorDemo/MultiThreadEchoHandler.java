67ecb651b863154cb35548a9a36294e150ab4cdf
xuexiao <xuexiao@belink.com>
2022-09-02 14:57:49 +0800

    完善多线程Reactor反应器模式代码

--- java_nio_lecture/src/main/java/org/cloudxue/ioDemo/ReactorDemo/MultiThreadEchoHandler.java
+++ java_nio_lecture/src/main/java/org/cloudxue/ioDemo/ReactorDemo/MultiThreadEchoHandler.java
@@ -4,6 +4,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.cloudxue.NioDemoConfig;
 
 import java.io.IOException;
+import java.net.StandardSocketOptions;
 import java.nio.ByteBuffer;
 import java.nio.channels.SelectionKey;
 import java.nio.channels.Selector;
@@ -13,7 +14,8 @@ import java.util.concurrent.Executors;
 
 /**
  * @ClassName MultiThreadEchoHandler
- * @Description 请描述类的业务用途
+ * @Description 多线程Reactor反应器模式业务处理类
+ * 通过线程池，将数据传输和业务处理的代码执行隔离，彻底地做到IO处理及业务处理线程和反应器IO事件轮询线程的完全隔离
  * @Author xuexiao
  * @Date 2021/11/29 下午3:25
  * @Version 1.0
@@ -28,25 +30,38 @@ public class MultiThreadEchoHandler implements Runnable{
     //引入线程池
     static ExecutorService pool = Executors.newFixedThreadPool(4);
 
-    MultiThreadEchoHandler(Selector selector, SocketChannel channel) throws IOException {
-        this.channel = channel;
+    MultiThreadEchoHandler(Selector selector, SocketChannel c) throws IOException {
+        this.channel = c;
         channel.configureBlocking(false);
+
+        channel.setOption(StandardSocketOptions.TCP_NODELAY, true);
+
         //先取得选择键，再设置感兴趣的IO事件
         sk = channel.register(selector, 0);
         //将自身Handler作为sk选择键的附件，方便事件dispatch
         sk.attach(this);
         //向sk选择键设置READ就绪事件
         sk.interestOps(SelectionKey.OP_READ);
+        //唤醒选择，使得OP_READ生效
         selector.wakeup();
     }
-    //此run方法在IO轮询事件中被调用
+
+    /**
+     * 此run方法在IO轮询事件中被调用
+     * 提交数据传输任务到线程池
+     * 使得IO业务处理不在IO事件轮询线程中执行，而是放到独立的线程池中执行
+     */
     @Override
     public void run() {
 //        pool.submit(() -> asyncRun());
         pool.execute(new AsyncTask());
     }
 
-    public synchronized  void asyncRun() {
+    /**
+     * IO操作和具体的业务处理类
+     * 为了避免发送和读取的状态混乱，此处使用synchronized进行安全处理
+     */
+    public synchronized void asyncRun() {
         try {
             if (status == SENDING) {
                 //写入通道
@@ -58,6 +73,7 @@ public class MultiThreadEchoHandler implements Runnable{
                 //写完成，进入接收状态
                 status = RECEIVING;
             } else if (status == RECEIVING) {
+                //从通道读
                 int readLen = 0;
                 while ((readLen = channel.read(byteBuffer)) > 0) {
                     log.info(new String(byteBuffer.array(), 0, readLen));
@@ -80,6 +96,9 @@ public class MultiThreadEchoHandler implements Runnable{
         }
     }
 
+    /**
+     * 异步任务内部类
+     */
     class AsyncTask implements Runnable {
         public void run () {
             MultiThreadEchoHandler.this.asyncRun();
--- java_nio_lecture/src/main/java/org/cloudxue/ioDemo/ReactorDemo/MultiThreadEchoServerReactor.java
+++ java_nio_lecture/src/main/java/org/cloudxue/ioDemo/ReactorDemo/MultiThreadEchoServerReactor.java
@@ -15,7 +15,7 @@ import java.util.concurrent.atomic.AtomicInteger;
 
 /**
  * @ClassName MultiThreadEchoServerReactor
- * @Description 多线程版本反应器模式实例
+ * @Description 多线程Reactor反应器模式实例
  * 两个选择器、两个子反应器，每个子反应器负责查询和分发一个选择器的IO事件
  * 服务端的监听通道（ServerSocketChannel）都注册到第一个选择器
  * Socket传输通道（SocketChannel）都注册到第二个选择器
@@ -28,41 +28,58 @@ import java.util.concurrent.atomic.AtomicInteger;
 public class MultiThreadEchoServerReactor {
     ServerSocketChannel serverSocketChannel;
     AtomicInteger next = new AtomicInteger(0);
-    //选择器数组，引入多个选择器
-    Selector[] selectors = new Selector[2];
+    //boss选择器，负责查询和分发新连接事件
+    Selector bossSelector = null;
+    //选择器数组，引入多个选择器，负责查询和分发IO传输事件
+    Selector[] workSelectors = new Selector[2];
+
+    SubReactor bossReactor = null;
     //引入多个子反应器
-    SubSelector[] subSelectors = null;
+    SubReactor[] workReactors = null;
 
     MultiThreadEchoServerReactor() throws IOException {
         //初始化多个选择器
-        selectors[0] = Selector.open();
-        selectors[1] = Selector.open();
+        bossSelector = Selector.open();
+        workSelectors[0] = Selector.open();
+        workSelectors[1] = Selector.open();
+
         serverSocketChannel = ServerSocketChannel.open();
         serverSocketChannel.configureBlocking(false);
+
         InetSocketAddress address = new InetSocketAddress(NioDemoConfig.SOCKET_SERVER_IP, NioDemoConfig.SOCKET_SERVER_PORT);
         serverSocketChannel.socket().bind(address);
         log.info("服务端已开始监听: " + address);
-        //第一个选择器，负责监听新连接事件
-        SelectionKey sk = serverSocketChannel.register(selectors[0], SelectionKey.OP_ACCEPT);
-        //绑定Handler：新连接监控Handler绑定到SelectionKey
+
+        //bossSelector负责监听新连接事件
+        SelectionKey sk = serverSocketChannel.register(bossSelector, SelectionKey.OP_ACCEPT);
+        //绑定Handler：新连接监听Handler绑定到SelectionKey
         sk.attach(new AcceptorHandler());
-        //第一个子反应器，负责第一个选择器的新连接事件分发（而不处理）
-        SubSelector subSelector1 = new SubSelector(selectors[0]);
-        //第二个子反应器，负责第二个选择器的传输事件分发（而不处理）
-        SubSelector subSelector2 = new SubSelector(selectors[1]);
-        subSelectors = new SubSelector[]{subSelector1, subSelector2};
+
+        //bossReactor子反应器，处理新连接的bossSelector
+        bossReactor = new SubReactor(bossSelector);
+
+        //第一个子反应器，负责一个work选择器的传输事件分发（而不处理）
+        SubReactor workReactor0 = new SubReactor(workSelectors[0]);
+        //第二个子反应器，负责一个work选择器的传输事件分发（而不处理）
+        SubReactor workReactor1 = new SubReactor(workSelectors[1]);
+        workReactors = new SubReactor[]{workReactor0, workReactor1};
     }
 
     private void startServer() {
-        new Thread(subSelectors[0]).start();
-        new Thread(subSelectors[1]).start();
+        //每个子反应器对应一条线程
+        new Thread(bossReactor).start();
+        new Thread(workReactors[0]).start();
+        new Thread(workReactors[1]).start();
     }
 
-    //子反应器，负责事件分发，但不负责事件处理
-    class SubSelector implements Runnable {
+    /**
+     * 子反应器内部类
+     * 负责事件分发，但不负责事件处理
+     */
+    class SubReactor implements Runnable {
         final Selector selector;
 
-        SubSelector(Selector selector) {
+        SubReactor(Selector selector) {
             this.selector = selector;
         }
 
@@ -70,8 +87,11 @@ public class MultiThreadEchoServerReactor {
         public void run() {
             try {
                 while (!Thread.interrupted()) {
-                    selector.select();
+                    selector.select(1000);
                     Set<SelectionKey> selectionKeys = selector.selectedKeys();
+                    if (null == selectionKeys || selectionKeys.size() == 0) {
+                        continue;
+                    }
                     Iterator<SelectionKey> iterator = selectionKeys.iterator();
                     while (iterator.hasNext()) {
                         SelectionKey sk = iterator.next();
@@ -103,11 +123,15 @@ public class MultiThreadEchoServerReactor {
                     log.info("接收到一个新连接");
                     int index = next.get();
                     log.info("选择器的编号： " + index);
-                    Selector selector = selectors[index];
+                    Selector selector = workSelectors[index];
                     new MultiThreadEchoHandler(selector, socketChannel);
                 }
             } catch (Exception e) {
                 e.printStackTrace();
+            }
+            //轮询策略
+            if (next.getAndIncrement() == workSelectors.length) {
+                next.set(0);
             }
         }
     }
