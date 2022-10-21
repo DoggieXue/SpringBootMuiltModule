599d321ac226db0d3ca4565eb51f56b7e160aa56
xuexiao <xuexiao@belink.com>
2022-10-21 14:53:41 +0800

    策略模式实现JSON序列化与反序列化

---
+++ democommon/src/main/java/org/cloudxue/common/json/JsonStrategy.java
@@ -0,0 +1,32 @@
+package org.cloudxue.common.json;
+
+import java.lang.reflect.Type;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * @ClassName JsonStrategy
+ * @Description 请描述类的业务用途
+ * @Author xuexiao
+ * @Date 2022/10/20 5:51 下午
+ * @Version 1.0
+ **/
+public interface JsonStrategy {
+    <K, V> Map<K, V> toMap(String json);
+
+    <K, V> Map<K, V> toMap(String json, Type type);
+
+    <T> List<T> toList(String json);
+
+    <T> List<T> toList(String json, Type type);
+
+    String toJson(Object object);
+
+    String toJson(Object object, String dateFormatPattern);
+
+    <T> T fromJson(String json, Class<T> valueType);
+
+    <K, V> Map<K, V> objectToMap(Object fromValue);
+
+    <T> T mapToObject(Map fromMap, Class<T> toValueType);
+}
