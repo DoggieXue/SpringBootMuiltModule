78ef264e7fde1ac172afa2ba1fc51e43fe03d415
xuexiao <xuexiao@belink.com>
2022-10-26 14:31:04 +0800

    新增OkHttp工具类

--- netty_lecture/src/test/java/HTTPKeepAliveTester.java
+++ netty_lecture/src/test/java/HTTPKeepAliveTester.java
@@ -1,7 +1,10 @@
 import org.cloudxue.common.util.HttpClientHelper;
 import org.cloudxue.common.util.JdkHttpURLConnectionHelper;
+import org.cloudxue.common.util.OkHttpUtil;
 import org.junit.Test;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
@@ -14,7 +17,8 @@ import java.util.concurrent.Executors;
  **/
 public class HTTPKeepAliveTester {
 
-    private String url = "http://10.238.144.120:8891/ifp-creditcard-service/ifp-api-docs.do";
+//    private String url = "http://10.238.144.120:8891/ifp-creditcard-service/ifp-api-docs.do";
+    private String url = "http://127.0.0.1:18899/";
     private ExecutorService pool = Executors.newFixedThreadPool(10);
 
     /**
@@ -24,25 +28,51 @@ public class HTTPKeepAliveTester {
     public void simpleGet() throws InterruptedException{
         int index = 1000000;
         while (--index > 0) {
+            String target = url + index;
             pool.submit(() -> {
-                String out = JdkHttpURLConnectionHelper.jdkGet(url);
+                String out = JdkHttpURLConnectionHelper.jdkGet(target);
                 System.out.println("out = " + out);
             });
         }
         Thread.sleep(Integer.MAX_VALUE);
     }
 
+    /**
+     *
+     * @throws InterruptedException
+     */
     @Test
     public void pooledGet() throws InterruptedException {
-        int index = 1000000;
+        int index = 10000;
         while (--index > 0) {
+            String target = url + index;
             pool.submit(() -> {
-               String out = HttpClientHelper.get(url);
-                System.out.println("out" + out);
+               String out = HttpClientHelper.get(target);
+                System.out.println("======out====" + out);
             });
         }
         Thread.sleep(Integer.MAX_VALUE);
     }
 
 
+    @Test
+    public void okHttpGet() throws InterruptedException{
+//        int index = 100;
+//        while (--index > 0) {
+//            String target = url + index;
+//            Map<String, String> headers = new HashMap<>();
+//            headers.put("Content-Type", "application/x-www-form-urlencoded");
+//            headers.put("User-Agent","PostmanRuntime/7.29.2");
+//            pool.submit(() -> {
+//                String out = OkHttpUtil.simpleGet(target,headers);
+//                System.out.println("out" + out);
+//            });
+//        }
+//        Thread.sleep(Integer.MAX_VALUE);
+        Map<String, String> headers = new HashMap<>();
+        headers.put("Content-Type", "application/x-www-form-urlencoded");
+        headers.put("User-Agent","PostmanRuntime/7.29.2");
+        String out = OkHttpUtil.simpleGet(url,headers);
+        System.out.println("out" + out);
+    }
 }
